fbd60ffe3d109ca2d26230633cad763f57e552e7
max_rad <maxim_radionov@bk.ru>
2021-09-03 10:15:20 +0300

    Выполнил автоматизацию (https://github.com/htmlacademy-adaptive/988381-mishka-23/pull/5#issuecomment-901169034, https://github.com/htmlacademy-adaptive/988381-mishka-23/pull/5#issuecomment-904721310)

---
+++ source/js/modal.js
@@ -0,0 +1,22 @@
+const mainPage = document.querySelector(".page__body");
+const modalLink = document.querySelector(".promo__button");
+const modalPopup = document.querySelector(".modal");
+const modalForm = modalPopup.querySelector(".modal__form");
+
+
+modalLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  modalPopup.classList.add("modal--show");
+  mainPage.classList.add("page__body--overlay");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.key === "Esc" || evt.key === "Escape") {
+    if (modalPopup.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modalPopup.classList.remove("modal--show");
+      mainPage.classList.remove("page__body--overlay");
+    }
+  }
+});
